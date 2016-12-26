c536e124e7b4bed94b91518b17e99e8b485ab1d9
sumith <sumithpd@gmail.com>
2016-12-26 14:31:05 +0000

    working greeter

--- public/bundle.js
+++ public/bundle.js
@@ -20457,13 +20457,18 @@ module.exports = require('./lib/React');
 //import React, { Component } from 'react';
 var React = require('react');
 var ReactDOM = require('react-dom');
+class Greeter extends React.Component {
+  render() {
+    return React.DOM.h1(null, "Hello" + this.props.name);
+  }
+}
+
 class App extends React.Component {
   render() {
-    return React.createElement(
-      'h1',
-      null,
-      'Hello World!'
-    );
+    return React.createElement(Greeter, { name: "Sumith" });
+    //  return React.DOM.h1(null, "Hello World from React.DOM");
+    //  return React.createElement('h1', null, "Hello World from CreateElement")
+    // return <h1>Hello World!</h1>;
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 //import React, { Component } from 'react';
 var React = require('react');
  var ReactDOM  = require('react-dom');
+class Greeter extends React.Component {
+  render() {
+    return React.DOM.h1(null, "Hello" + this.props.name); 
+  }
+}
+ 
 class App extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return React.createElement(Greeter, { name:"Sumith"});
+  //  return React.DOM.h1(null, "Hello World from React.DOM");
+  //  return React.createElement('h1', null, "Hello World from CreateElement")
+   // return <h1>Hello World!</h1>;
   }
 }
 
